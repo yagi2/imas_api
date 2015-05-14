6df4e1752a0e0277cf4af1eb9db06cae873ce955
Itsuki Aoyagi <mail@yagi2.com>
2015-05-14 11:18:04 +0900

    change type

--- SQL/imas/character_765.sql
+++ SQL/imas/character_765.sql
@@ -7,94 +7,94 @@ USE `fuel_dev`;
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '天海春香', 'あまみはるか', '天海', 'あまみ', '春香', 'はるか', 4, 3, 1, true, 'O', '赤', '中村繪里子', 'なかむらえりこ', '中村', 'なかむら', '繪里子', 'えりこ', 11, 19, 1, 'えりりん');
+       ('765プロダクション', '天海春香', 'あまみはるか', '天海', 'あまみ', '春香', 'はるか', 4, 3, 1, true, 'O', '赤', '中村繪里子', 'なかむらえりこ', '中村', 'なかむら', '繪里子', 'えりこ', 11, 19, 1, 'えりりん');
 
 -- 如月千早
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '如月千早', 'きさらぎちはや', '如月', 'きさらぎ', '千早', 'ちはや', 2, 25, 1, true, 'A', '青', '今井麻美', 'いまいあさみ', '今井', 'いまい', '麻美', 'あさみ', 5, 16, 1, 'ミンゴス');
+       ('765プロダクション', '如月千早', 'きさらぎちはや', '如月', 'きさらぎ', '千早', 'ちはや', 2, 25, 1, true, 'A', '青', '今井麻美', 'いまいあさみ', '今井', 'いまい', '麻美', 'あさみ', 5, 16, 1, 'ミンゴス');
 
 -- 萩原雪歩
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '萩原雪歩', 'はぎわらゆきほ', '萩原', 'はぎわら', '雪歩', 'ゆきほ', 12, 24, 1, true, 'A', '白', '浅倉杏美', 'あさくらあずみ', '浅倉', 'あさくら', '杏美', 'あずみ', 2, 15, 1, 'あずみん');
+       ('765プロダクション', '萩原雪歩', 'はぎわらゆきほ', '萩原', 'はぎわら', '雪歩', 'ゆきほ', 12, 24, 1, true, 'A', '白', '浅倉杏美', 'あさくらあずみ', '浅倉', 'あさくら', '杏美', 'あずみ', 2, 15, 1, 'あずみん');
 
 -- 高槻やよい
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '高槻やよい', 'たかつきやよい', '高槻', 'たかつき', 'やよい', 'やよい', 3, 25, 1, true, 'O', 'オレンジ', '仁後真耶子', 'にごまやこ', '仁後', 'にご', '真耶子', 'まやこ', 12, 2, 1, 'まやちゃん');
+       ('765プロダクション', '高槻やよい', 'たかつきやよい', '高槻', 'たかつき', 'やよい', 'やよい', 3, 25, 1, true, 'O', 'オレンジ', '仁後真耶子', 'にごまやこ', '仁後', 'にご', '真耶子', 'まやこ', 12, 2, 1, 'まやちゃん');
 
 -- 秋月律子
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '秋月律子', 'あきづきりつこ', '秋月', 'あきづき', '律子', 'りつこ', 6, 23, 1, true, 'A', '緑', '若林直美', 'わかばやしなおみ', '若林', 'わかばやし', '直美', 'なおみ', 11, 2, 1, '直ちゃん');
+       ('765プロダクション', '秋月律子', 'あきづきりつこ', '秋月', 'あきづき', '律子', 'りつこ', 6, 23, 1, true, 'A', '緑', '若林直美', 'わかばやしなおみ', '若林', 'わかばやし', '直美', 'なおみ', 11, 2, 1, '直ちゃん');
 
 -- 水瀬伊織
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '水瀬伊織', 'みなせいおり', '水瀬', 'みなせ', '伊織', 'いおり', 5, 5, 1, true, 'AB', 'ピンク', '釘宮理恵', 'くぎみやりえ', '釘宮', 'くぎみや', '理恵', 'りえ', 5, 30, 1, 'くぎゅ');
+       ('765プロダクション', '水瀬伊織', 'みなせいおり', '水瀬', 'みなせ', '伊織', 'いおり', 5, 5, 1, true, 'AB', 'ピンク', '釘宮理恵', 'くぎみやりえ', '釘宮', 'くぎみや', '理恵', 'りえ', 5, 30, 1, 'くぎゅ');
 
 -- 三浦あずさ
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '三浦あずさ', 'みうらあずさ', '三浦', 'みうら', 'あずさ', 'あずさ', 7, 19, 1, true, 'O', '紫', 'たかはし智秋', 'たかはしちあき', 'たかはし', 'たかはし', '智秋', 'ちあき', 5, 8, 1, 'チアキんぐ');
+       ('765プロダクション', '三浦あずさ', 'みうらあずさ', '三浦', 'みうら', 'あずさ', 'あずさ', 7, 19, 1, true, 'O', '紫', 'たかはし智秋', 'たかはしちあき', 'たかはし', 'たかはし', '智秋', 'ちあき', 5, 8, 1, 'チアキんぐ');
 
 -- 双海亜美
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '双海亜美', 'ふたみあみ', '双海', 'ふたみ', '亜美', 'あみ', 5, 22, 1, true, 'B', '黄色', '下田麻美', 'しもだあさみ', '下田', 'しもだ', '麻美', 'あさみ', 1, 30, 1, 'あさぽん');
+       ('765プロダクション', '双海亜美', 'ふたみあみ', '双海', 'ふたみ', '亜美', 'あみ', 5, 22, 1, true, 'B', '黄色', '下田麻美', 'しもだあさみ', '下田', 'しもだ', '麻美', 'あさみ', 1, 30, 1, 'あさぽん');
 
 -- 双海真美
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '双海真美', 'ふたみまみ', '双海', 'ふたみ', '真美', 'まみ', 5, 22, 1, true, 'B', '黄色', '下田麻美', 'しもだあさみ', '下田', 'しもだ', '麻美', 'あさみ', 1, 30, 1, 'あさぽん');
+       ('765プロダクション', '双海真美', 'ふたみまみ', '双海', 'ふたみ', '真美', 'まみ', 5, 22, 1, true, 'B', '黄色', '下田麻美', 'しもだあさみ', '下田', 'しもだ', '麻美', 'あさみ', 1, 30, 1, 'あさぽん');
 
 -- 菊地真
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '菊地真', 'きくちまこと', '菊地', 'きくち', '真', 'まこと', 8, 29, 1, true, 'O', '黒', '平田宏美', 'ひらたひろみ', '平田', 'ひらた', '宏美', 'ひろみ', 2, 19, 1, 'ひろりん');
+       ('765プロダクション', '菊地真', 'きくちまこと', '菊地', 'きくち', '真', 'まこと', 8, 29, 1, true, 'O', '黒', '平田宏美', 'ひらたひろみ', '平田', 'ひらた', '宏美', 'ひろみ', 2, 19, 1, 'ひろりん');
 
 -- 星井美希
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '星井美希', 'ほしいみき', '星井', 'ほしい', '美希', 'みき', 11, 23, 1, true, 'B', '若草', '長谷川明子', 'はせがわあきこ', '長谷川', 'はせがわ', '明子', 'あきこ', 4, 14, 1, 'アッキー');
+       ('765プロダクション', '星井美希', 'ほしいみき', '星井', 'ほしい', '美希', 'みき', 11, 23, 1, true, 'B', '若草', '長谷川明子', 'はせがわあきこ', '長谷川', 'はせがわ', '明子', 'あきこ', 4, 14, 1, 'アッキー');
 
 -- 我那覇響
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '我那覇響', 'がなはひびき', '我那覇', 'がなは', '響', 'ひびき', 10, 10, 1, true, 'A', '浅葱色', '沼倉愛美', 'ぬまくらまなみ', '沼倉', 'ぬまくら', '愛美', 'まなみ', 4, 15, 1, 'ぬーぬー');
+       ('765プロダクション', '我那覇響', 'がなはひびき', '我那覇', 'がなは', '響', 'ひびき', 10, 10, 1, true, 'A', '浅葱色', '沼倉愛美', 'ぬまくらまなみ', '沼倉', 'ぬまくら', '愛美', 'まなみ', 4, 15, 1, 'ぬーぬー');
 
 -- 四条貴音
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '四条貴音', 'しじょうたかね', '四条', 'しじょう', '貴音', 'たかね', 1, 21, 1, true, 'B', '臙脂色', '原由実', 'はらゆみ', '原', 'はら', '由実', 'ゆみ', 1, 21, 1, 'はらみー');
+       ('765プロダクション', '四条貴音', 'しじょうたかね', '四条', 'しじょう', '貴音', 'たかね', 1, 21, 1, true, 'B', '臙脂色', '原由実', 'はらゆみ', '原', 'はら', '由実', 'ゆみ', 1, 21, 1, 'はらみー');
 
 -- 高木順一朗
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '高木順一朗', 'たかぎじゅんいちろう', '高木', 'たかぎ', '純一郎', 'じゅんいちろう', 7, 6, 0, false, 'AB', ' ', '徳丸完', 'とくまるかん', '徳丸', 'とくまる', '完', 'かん', 5, 26, 0, ' ');
+       ('765プロダクション', '高木順一朗', 'たかぎじゅんいちろう', '高木', 'たかぎ', '純一郎', 'じゅんいちろう', 7, 6, 0, false, 'AB', ' ', '徳丸完', 'とくまるかん', '徳丸', 'とくまる', '完', 'かん', 5, 26, 0, ' ');
 
 -- 高木順二朗
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '高木順二朗', 'たかぎじゅんじろう', '高木', 'たかぎ', '順二朗', 'じゅんじろう', 7, 6, 0, false, 'B', ' ', '大塚芳忠', 'おおつかほうちゅう', '大塚', 'おおつか', '芳忠', 'ほうちゅう', 5, 19, 1, 'ほっちゅん');
+       ('765プロダクション', '高木順二朗', 'たかぎじゅんじろう', '高木', 'たかぎ', '順二朗', 'じゅんじろう', 7, 6, 0, false, 'B', ' ', '大塚芳忠', 'おおつかほうちゅう', '大塚', 'おおつか', '芳忠', 'ほうちゅう', 5, 19, 1, 'ほっちゅん');
 
 -- 音無小鳥
 INSERT INTO `imas_characters`
        (`type`, `ch_name`, `ch_name_ruby`, `ch_family_name`, `ch_family_name_ruby`, `ch_first_name`, `ch_first_name_ruby`, `ch_birth_month`, `ch_birth_day`, `ch_gender`, `is_idol`, `ch_blood_type`, `ch_color`, `cv_name`, `cv_name_ruby`, `cv_family_name`, `cv_family_name_ruby`, `cv_first_name`, `cv_first_name_ruby`, `cv_birth_month`, `cv_birth_day`, `cv_gender`, `cv_nickname`)
        VALUES
-       ('765', '音無小鳥', 'おとなしことり', '音無', 'おとなし', '小鳥', 'ことり', 9, 9, 1, false, 'AB', 'ひよこ色', '滝田樹里', 'たきたじゅり', '滝田', 'たきた', '樹里', 'じゅり', 4, 18, 1, '樹里きち');
+       ('765プロダクション', '音無小鳥', 'おとなしことり', '音無', 'おとなし', '小鳥', 'ことり', 9, 9, 1, false, 'AB', 'ひよこ色', '滝田樹里', 'たきたじゅり', '滝田', 'たきた', '樹里', 'じゅり', 4, 18, 1, '樹里きち');

6cc1ec86389595d1036a90df4f46352f70e41664
Itsuki Aoyagi <mail@yagi2.com>
2015-05-19 07:40:05 +0900

    add vagrant and fix #1

--- fuel/app/classes/controller/imas/character.php
+++ fuel/app/classes/controller/imas/character.php
@@ -15,69 +15,74 @@ class Controller_Imas_Character extends Controller_Rest
       $result->$key = $val;
     }
     return $result;
-  }
-
+  } 
+ 
   public function get_list()
   {
+    // そのうちcheck_result()とresult_check()は共通部分に移す
+    // check_result() : 結果で一致一致しないものを削除
+    function check_result($param_name, $res_array, $res_array_tmp, $params) {
+      if (empty($res_array)) {
+        $res_array = $res_array_tmp;
+      }
+      else {
+        foreach ($res_array as $key => $value) {
+          if ($value[$param_name] != $params[$param_name]) unset($res_array[$key]);
+        }
+      }
+      return $res_array;
+    }
+    // result_get() : DBからする結果を取得する
+    function result_get($params, $param_key, $res_array) {
+      if ($params[$param_key] != null) {
+        $query = DB::select()->from('imas_characters')->where($param_key, $params[$param_key]);
+        $res_tmp = $query->execute()->as_array();
+        
+        $res_array = check_result($param_key, $res_array, $res_tmp, $params);
+        
+        return $res_array;
+      }
+      return $res_array;
+    }
+    
     $ipt = Input::all();
     $arr = array_filter($ipt, 'strlen');
 
-    $params['id']         = Input::get('id',         null);
-    $params['type']       = Input::get('type',       null);
-    $params['c_name']     = Input::get('c_name',     null);
-    $params['c_month']    = Input::get('c_month',    null);
-    $params['c_day']      = Input::get('c_day',      null);
-    $params['c_gender']   = Input::get('c_gender',   null);
-    $params['is_idol']    = Input::get('is_idol',    null);
-    $params['c_blood']    = Input::get('c_blood',    null);
-    $params['c_color']    = Input::get('c_color',    null);
-    $params['v_name']     = Input::get('v_name',     null);
-    $params['v_month']    = Input::get('v_month',    null);
-    $params['v_day']      = Input::get('v_day',      null);
-    $params['v_gender']   = Input::get('v_gender',   null);
-    $params['v_nichname'] = Input::get('v_nickname', null);
-
-    if (empty($arr)) $this->response(Model_Imas_Character::find('all'));
-    if ($params['id'] != null){
-      $query = DB::select()->from('imas_characters')->where('id', $params['id']);
-      $res   = $query->execute()->as_array();
-      $this->response($res);
-    }
-    if ($params['c_name'] != null) {
-      $query = DB::select()->from('imas_characters')->where('ch_name', $params['c_name']);
-      $res   = $query->execute()->as_array();
-      $this->response($res);
-    }
-
-    /*
-    if ($params['type'] != null){
-      $query = DB::select()->from('imas_characters')->where('type', $params['type']);
-      $res   = $query->execute()->as_array();
-      $this->response($res);
-      }
-    if ($params['c_month'] != null){
-      $query = DB::select()->from('imas_characters')->where('ch_birth_month', $params['c_month']);
-      $res   = $query->execute()->as_array();
-      $this->response($res);
-    }
-    */
-
-    /*
-     if (!empty($arr)){
-      if ($params['id'] != null){
-	$id = Model_Imas_Character::find(array('id' => $params['id']));
-      }
-      if ($params['type'] != null){
-	$type = Model_Imas_Character::find(array('type' => $params['type']));	  
-      }
+    $params['id']             = Input::get('id',             null);
+    $params['type']           = Input::get('type',           null);
+    $params['ch_name']        = Input::get('ch_name',        null);
+    $params['ch_birth_month'] = Input::get('ch_birth_month', null);
+    $params['ch_birth_day']   = Input::get('ch_birth_day',   null);
+    $params['ch_gender']      = Input::get('ch_gender',      null);
+    $params['is_idol']        = Input::get('is_idol',        null);
+    $params['ch_blood_type']  = Input::get('ch_blood_type',  null);
+    $params['ch_color']       = Input::get('ch_color',       null);
+    $params['cv_name']        = Input::get('cv_name',        null);
+    $params['cv_birth_month'] = Input::get('cv_birth_month', null);
+    $params['cv_birth_day']   = Input::get('cv_birth_day',   null);
+    $params['cv_gender']      = Input::get('cv_gender',      null);
+    $params['cv_nickname']    = Input::get('cv_nickname',    null);
+   
+    $res = array();
     
-      $this->object_merge($id, $type, $characters);
-      $this->response($characters);
-    }
-    else {
-    */
-    //      $characters = Model_Imas_Character::find('all');
-    // $this->response($characters);
-      // }
+    // パラメータを渡さない場合はすべて返す
+    if (empty($arr)) $this->response(Model_Imas_Character::find('all'));
+    
+    $res = result_get($params, "id",             $res);
+    $res = result_get($params, "type",           $res);
+    $res = result_get($params, "ch_name",        $res);
+    $res = result_get($params, "ch_birth_month", $res);
+    $res = result_get($params, "ch_birth_day",   $res);
+    $res = result_get($params, "ch_gender",      $res);
+    $res = result_get($params, "is_idol",        $res);
+    $res = result_get($params, "ch_blood_type",  $res);
+    $res = result_get($params, "ch_color",       $res);
+    $res = result_get($params, "cv_name",        $res);
+    $res = result_get($params, "cv_birth_month", $res);
+    $res = result_get($params, "cv_birth_day",   $res);
+    $res = result_get($params, "cv_gender",      $res);
+    $res = result_get($params, "cv_nickname",    $res);
+
+    $this->response($res);
   }
 }
--- fuel/app/classes/model/imas/character.php
+++ fuel/app/classes/model/imas/character.php
@@ -3,30 +3,30 @@
 class Model_Imas_Character extends \Orm\Model
 {
 	protected static $_properties = array(
-		'id',
-		'type',
-		'ch_name',
-	        'ch_name_ruby',
+		'id',                   //
+		'type',					//
+		'ch_name',				//
+	    'ch_name_ruby',
 		'ch_family_name',
 		'ch_family_name_ruby',
 		'ch_first_name',
 		'ch_first_name_ruby',
-		'ch_birth_month',
-		'ch_birth_day',
-		'ch_gender',
-		'is_idol',
-		'ch_blood_type',
-		'ch_color',
-		'cv_name',
+		'ch_birth_month',		//
+		'ch_birth_day',			//
+		'ch_gender',			//
+		'is_idol',				//
+		'ch_blood_type',		//
+		'ch_color',				//
+		'cv_name',				//
 		'cv_name_ruby',
 		'cv_family_name',
 		'cv_family_name_ruby',
 		'cv_first_name',
 		'cv_first_name_ruby',
-		'cv_birth_month',
-		'cv_birth_day',
-		'cv_gender',
-		'cv_nickname',
+		'cv_birth_month',		//
+		'cv_birth_day',			//
+		'cv_gender',			//
+		'cv_nickname',			//
 	);
 	protected static $_table_name = 'imas_characters';
 

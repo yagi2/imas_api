a21558b5d017176ba04f00a14eef36d8e0e8d0ad
Itsuki Aoyagi <mail@yagi2.com>
2015-06-28 01:33:23 +0900

    うなすけのシャンプーが全世界にバレる所だった。

--- SQL/shampoo/datas/data_cv.sql
+++ SQL/shampoo/datas/data_cv.sql
@@ -7,12 +7,3 @@ INSERT INTO `sh_cv_list`
        VALUES
        (1, '沼倉愛美');
        
-INSERT INTO `sh_cv_list`
-       (`sh_id`, `name`)
-       VALUES
-       (1, 'やぎにいちゃん');
-       
-INSERT INTO `sh_cv_list`
-       (`sh_id`, `name`)
-       VALUES
-       (2, 'うなすけ');
